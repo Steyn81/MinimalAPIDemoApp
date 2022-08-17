a03b52eaeb79872ec8e8cd664139d11c3ec2621a
dsteyn.software@gmail.com <dsteyn.software@gmail.com>
2022-08-17 19:38:44 +0200

    Add project files.

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using System.Data;
+
+namespace DataAccess.DbAccess;
+public class SqlDataAccess : ISqlDataAccess
+{
+	//this class talks to sql through dapper
+
+	private readonly IConfiguration _config;
+
+	public SqlDataAccess(IConfiguration config)
+	{
+		_config = config;
+	}
+
+	//1 - Connect to sql
+	//2 - Executes query
+	//3 - Return IEnumerable
+	public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default")
+	{
+		using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+		return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+	}
+
+	public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default")
+	{
+		using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+		await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+	}
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists ( select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Deon','Steyn'),
+	('Cassy','Wyngaard'),
+	('Pieter','Steyn'),
+	('Marion','Steyn');
+end
